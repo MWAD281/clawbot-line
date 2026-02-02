f1afe9b9c1e29c21ab3832f7378310e6a09880b1
MWAD281 <107363303+MWAD281@users.noreply.github.com>
2026-02-02 18:30:29 +0800

    Update debate.py

--- world/debate.py
+++ world/debate.py
@@ -1,17 +1,21 @@
 # world/debate.py
 
 from agents.ceo_alpha import ceo_alpha
+from agents.ceo_beta import ceo_beta
 
 def run_ceo_debate(user_input: str, world_state: dict):
     """
     เรียก CEO ทุกคนมาโหวต
-    (ตอนนี้ยังมีแค่ Alpha)
     """
 
     votes = []
 
     votes.append(
         ceo_alpha(user_input, world_state)
+    )
+
+    votes.append(
+        ceo_beta(user_input, world_state)
     )
 
     return votes
