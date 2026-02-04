69e18bc1f73af24cf79d6e41ecdd5abf183c9c15
MWAD281 <107363303+MWAD281@users.noreply.github.com>
2026-02-04 09:46:05 +0800

    Update base.py

--- policies/base.py
+++ policies/base.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
-
+from clawbot.core.decision import Decision
 
 class Policy(ABC):
+
     @abstractmethod
-    def decide(self, world):
+    def decide(self, world_state: dict) -> Decision:
         pass
